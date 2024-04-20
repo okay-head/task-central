4eb633e1fe5d341c51a531f1585a3a2fb597895d
okay-head <68184623+okay-head@users.noreply.github.com>
2024-04-20 14:14:56 +0530

    feat: add search

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -53,29 +53,8 @@ export default function Navbar() {
             className='btn btn-ghost btn-xs sm:btn-sm sm:mx-2'
           >
             <List size={20} />
-            {/* <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth='1.5'
-              stroke='currentColor'
-              className='h-5 w-5'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z'
-              />
-            </svg> */}
             <span className='hidden md:inline-block'>All tasks</span>
           </Link>
-          <div className='form-control'>
-            <input
-              type='text'
-              placeholder='Search'
-              className='input input-sm input-bordered hidden w-24 sm:inline-block md:w-auto'
-            />
-          </div>
           <div className='dropdown dropdown-end'>
             <div
               tabIndex={0}
--- src/components/user/Tasks.tsx
+++ src/components/user/Tasks.tsx
@@ -7,6 +7,23 @@ import { AxiosError } from 'axios'
 
 export default function Tasks() {
   const [tasks, setTasks] = useState<TMongoObject[]>([])
+  const [query, setQuery] = useState('')
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setQuery(e.target.value)
+
+  // debounce inside useCallback
+  const searchFn = (taskList: TMongoObject[]) => {
+    if (query == '') return taskList
+
+    // handle filter
+    const newArr = taskList.filter(
+      (task) =>
+        task.title.toLowerCase().includes(query) ||
+        task.description.toLowerCase().includes(query),
+    )
+    return newArr
+  }
 
   const getTasks = async () => {
     try {
@@ -22,9 +39,9 @@ export default function Tasks() {
 
   useEffect(() => {
     const local = sessionStorage.getItem('firstLoad')
+    //only show this on the very first render
     if (!local) {
       toast.loading(
-        //only show this on the very first render
         <span className='text-sm'>
           Fetching data...
           <br />
@@ -40,12 +57,23 @@ export default function Tasks() {
 
   return (
     <Container>
-      <h1 className='mb-10 w-full border-b pb-4 text-4xl   '>Tasks</h1>
+      <div className='mb-10 flex w-full items-end justify-between border-b pb-4'>
+        <h1 className='text-4xl'>Tasks</h1>
+        <div className='form-control'>
+          <input
+            onChange={handleSearch}
+            value={query}
+            type='text'
+            placeholder='Search'
+            className='input input-sm input-bordered hidden w-24 sm:inline-block md:w-auto'
+          />
+        </div>
+      </div>
       <div className='tasks-container flex grid-cols-2 flex-col place-content-stretch gap-x-4 gap-y-8 sm:flex-row md:grid'>
         {tasks?.length === 0 ? (
           <h2 className='text-lg'>No tasks</h2>
         ) : (
-          tasks.map((x) => {
+          searchFn(tasks).map((x) => {
             return (
               <TaskCard
                 key={x._id}
