a2379fbe290b3f7d4d0608beff6e2dc8c1452685
okay-head <68184623+okay-head@users.noreply.github.com>
2024-05-04 19:08:06 +0530

    fix: cold start notif

--- src/components/user/Tasks.tsx
+++ src/components/user/Tasks.tsx
@@ -1,7 +1,7 @@
 import Container from '../shared/Container'
 import TaskCard from './TaskCard'
 import { getAllFn } from '../api/apiCalls'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useDebouncedCallback } from 'use-debounce'
 import { AxiosError } from 'axios'
@@ -9,6 +9,9 @@ import { AxiosError } from 'axios'
 export default function Tasks() {
   const [tasks, setTasks] = useState<TMongoObject[]>([])
 
+  const toastRef = useRef<null | string>(null)
+
+  // setting up search query
   // decoupling controlled input state from filter query
   const [input, setInput] = useState('')
   const [query, setQuery] = useState('')
@@ -28,24 +31,29 @@ export default function Tasks() {
       task.description.toLowerCase().includes(query),
   )
 
-  const getTasks = async () => {
-    try {
-      const response = await getAllFn()
-      setTasks(response)
-    } catch (error) {
-      // needlessly complicated because- types
-      let err: AxiosError | string = error as AxiosError
-      err = err.code || 'Unknown error. Check console'
-      toast.error(err)
-      console.log(error)
+  // Data fetching
+  useEffect(() => {
+    const getTasks = async () => {
+      try {
+        const response = await getAllFn()
+        setTasks(response)
+      } catch (error) {
+        // needlessly complicated because- types
+        let err: AxiosError | string = error as AxiosError
+        err = err.code || 'Unknown error. Check console'
+        toast.error(err)
+        console.log(error)
+      }
     }
-  }
+    getTasks()
+  }, [])
 
+  // Cold start notification
   useEffect(() => {
     const local = sessionStorage.getItem('firstLoad')
     //only show this on the very first render
     if (!local) {
-      toast.loading(
+      toastRef.current = toast.loading(
         <span className='text-sm'>
           Fetching data...
           <br />
@@ -54,10 +62,15 @@ export default function Tasks() {
         </span>,
       )
     }
-
+    // technically it shouldn't be a check of empty array, should set the array default state to null instead of []
+    if (tasks.length != 0 && toastRef.current) {
+      toast.dismiss(toastRef.current)
+      toastRef.current = null
+    }
     sessionStorage.setItem('firstLoad', 'true')
-    getTasks()
-  }, [])
+
+    return () => {}
+  }, [tasks.length])
 
   return (
     <Container>
