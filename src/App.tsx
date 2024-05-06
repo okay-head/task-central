c152cbec3c9921902f81b4d5fbc340558738554d
okay-head <68184623+okay-head@users.noreply.github.com>
2024-05-06 12:46:32 +0530

    pkg: add zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/nav/Navbar'
 import SignUp from './components/auth/SignUp'
@@ -7,21 +7,51 @@ import NotFound from './components/NotFound'
 import SignIn from './components/auth/SignIn'
 import Tasks from './components/user/Tasks'
 import CreateEditShell from './components/user/CreateEditShell'
-
-//   import { useEffect } from 'react'
-// import 'preline/preline'
-// import { IStaticMethods } from 'preline/preline'
-// import SignUp from './components/auth/SignUp'
-// import SignIn from './components/auth/SignIn'
-// import Home from './components/feed/Home'
-// import NotFound from './NotFound'
-// import Users from './components/users/Users'
-// import Profile from './components/profile/Profile'
-// import CheckAuth from './components/auth/CheckAuth'
-// import CreateWave from './components/user/CreateWave'
-// import Logout from './components/auth/Logout'
+import { useEffect, useRef, useState } from 'react'
+import { pingFn } from './components/api/apiCalls'
 
 export default function App() {
+  const [ready, setReady] = useState(false)
+  const toastRef = useRef<null | string>(null)
+  const toastRef2 = useRef<null | string>(null)
+  // i feel all of this should be app state
+
+  useEffect(() => {
+    // ping the API the first thing, to overcome cold start
+    pingFn()
+      .then(() => setReady(true))
+      .catch((err) => err)
+  }, [])
+
+  // Cold start notification
+  useEffect(() => {
+    const local = sessionStorage.getItem('firstLoad')
+    //only show this on the very first render
+    if (!local) {
+      toastRef.current = toast.loading(
+        <span className='text-sm'>
+          Fetching data...
+          <br />
+          Can take upto a minute or two due to cold starts of the dev
+          environment
+        </span>,
+      )
+      toastRef2.current = toast.loading(
+        <span className='text-sm'>Please wait...</span>,
+      )
+    }
+    if (toastRef.current && ready && toastRef2.current) {
+      toast.dismiss(toastRef.current)
+      toastRef.current = null
+
+      toast.dismiss(toastRef2.current)
+      toastRef2.current = null
+    }
+    sessionStorage.setItem('firstLoad', 'true')
+
+    return () => {}
+  }, [ready])
+
   return (
     <div className='app'>
       <Toaster />
